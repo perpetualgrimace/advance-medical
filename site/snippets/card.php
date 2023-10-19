a278a976febb110f7890cb3e1571133563d67ff6
perpetualgrimace <hello@jamesferrell.me>
2023-10-19 17:19:27 -0400

    fixes card on list page

--- site/snippets/card.php
+++ site/snippets/card.php
@@ -10,7 +10,9 @@
   // get thumbnail image
   $blog = $pages->find('blog');
   $placeholder = $blog->placeholderImg();
-  $placeholderUrl = $blog->url() . '/' . $placeholder;
+  $placeholderUrl = $site->url() . '/blog/' . $placeholder;
+
+  $iconFilename = $contentType;
 
   if ($contentType == 'blog-video') {
     // custom thumbnail image
@@ -48,37 +50,31 @@
 <article class="<?= $contentType ?>-card-container card-container g-col <?= $cardLayout ?>" data-category="<?= $categorySanitized ?>" data-type="<?= $contentType ?>">
   <div class="<?= $contentType ?>-card-inner card-inner">
 
-    <?php if ($thumbImg != $placeholderUrl): ?>
-      <a href="<?= $item->url() ?>" class="<?= $contentType ?>-card-thumb card-thumb" tabindex="-1">
-        <img class="<?= $contentType ?>-card-img card-img" data-src="<?= $thumbImg ?>" alt="" draggable="false">
-        <noscript>
-          <img class="<?= $contentType ?>-card-img card-img" src="<?= $thumbImg ?>" alt="" draggable="false">
-        </noscript>
-          <?php if ($contentType == 'blog-video') {
-            snippet('retina-img', [
-              'baseUrl' => $site->url() . '/assets/img/icons/',
-              'filename' => 'play',
-              'class' => 'blog-video-card-thumb-icon u-margin-top-off'
-            ]);
-          } ?>
-      </a>
-    <?php endif ?>
+    <a href="<?= $item->url() ?>" class="<?= $contentType ?>-card-thumb card-thumb" tabindex="-1">
+      <img class="<?= $contentType ?>-card-img card-img" data-src="<?= $thumbImg ?>" alt="" draggable="false">
+      <noscript>
+        <img class="<?= $contentType ?>-card-img card-img" src="<?= $thumbImg ?>" alt="" draggable="false">
+      </noscript>
+      <?php if ($contentType == 'blog-video') {
+        snippet('retina-img', [
+          'baseUrl' => $site->url() . '/assets/img/icons/',
+          'filename' => 'play',
+          'class' => 'blog-video-card-thumb-icon u-margin-top-off'
+        ]);
+      } ?>
+    </a>
 
     <div class="<?= $contentType ?>-card-caption card-caption u-margin-top-off">
-       <a class="<?= $contentType ?>-card-title card-title display u-left" href="<?= $item->url() ?>">
-         <?php // if ($contentType != 'blog-video') {
-            snippet('svg-img', [
-              'filename' => 'icons/' . $contentType,
-              'class' => 'card-icon'
-            ]);
-          // } ?> <?= $item->title() ?>
-       </a>
-       <?php if ($category != NULL): ?>
+      <a class="<?= $contentType ?>-card-title card-title display u-left" href="<?= $item->url() ?>">
+        <?php snippet('svg-img', [
+            'filename' => "icons/" . $iconFilename,
+            'class' => 'card-icon'
+        ]) ?> <?= $item->title() ?>
+      </a>
+      <?php if ($category != NULL): ?>
         <p class="<?= $contentType ?>-card-meta card-meta milli">Posted in <a href="<?php e($page->intendedTemplate() != 'blog-list', $site->url() . '/blog') ?>#<?= $categorySanitized ?>" class="card-link card-meta-link" tabindex="-1" data-category-trigger><?= $category ?></a></p>
       <?php endif ?>
-      <?php if ($thumbImg == $placeholderUrl): ?>
-        <a href="<?= $item->url() ?>" class="<?= $contentType ?>-card-link card-link card-more-link milli" tabindex="-1">View post &rsaquo;</a>
-      <?php endif; ?>
+      <a href="<?= $item->url() ?>" class="<?= $contentType ?>-card-link card-link card-more-link milli" tabindex="-1">View post &rsaquo;</a>
     </div>
 
   </div>
