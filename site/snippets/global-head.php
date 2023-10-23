48f385160e9d65d92a673f64ebfaf55b1d4f7192
perpetualgrimace <hello@jamesferrell.me>
2023-10-23 14:42:50 -0400

    updates readme & hides site from search engines

--- site/snippets/global-head.php
+++ site/snippets/global-head.php
@@ -2,12 +2,12 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="<?= $site->language()->code() ?>"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?= $site->language()->code() ?? "en" ?>"> <!--<![endif]-->
 <head>
 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <!--<meta name="google-site-verification" content="google-verification-here">-->
+  <meta name="robots" content="noindex" />
 
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
