a0b4afefaffc5f6e18360b6feb03c5b6e67bac4c
perpetualgrimace <hello@jamesferrell.me>
2023-10-24 18:09:05 -0400

    this time for sure (please make it stop)

--- site/snippets/retina-img.php
+++ site/snippets/retina-img.php
@@ -3,7 +3,7 @@
 if (isset($class)) { $class = $class; } else { $class = NULL; }
 if (isset($filename)) { $filename = $filename; } else { $filename = NULL; }
 if (isset($extension)) { $extension = $extension; } else { $extension = 'png'; }
-if (isset($baseUrl)) { $baseUrl = $baseUrl; } else { $baseUrl = '/media/pages' . $page->url() . '/'; }
+if (isset($baseUrl)) { $baseUrl = $baseUrl; } else { $baseUrl = '/' . $page->url() . '/media/pages/'; }
 
 $image1x = $baseUrl . $filename . '.' .  $extension;
 $image2x = $baseUrl . $filename . '@2x.' .  $extension;
