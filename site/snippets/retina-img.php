dac740fe451239adaae3cc12416698dba7a52fac
perpetualgrimace <hello@jamesferrell.me>
2023-10-25 14:21:03 -0400

    okay I was tired, this path should work

--- site/snippets/retina-img.php
+++ site/snippets/retina-img.php
@@ -3,7 +3,7 @@
 if (isset($class)) { $class = $class; } else { $class = NULL; }
 if (isset($filename)) { $filename = $filename; } else { $filename = NULL; }
 if (isset($extension)) { $extension = $extension; } else { $extension = 'png'; }
-if (isset($baseUrl)) { $baseUrl = $baseUrl; } else { $baseUrl = '/' . $page->url() . '/media/pages/'; }
+if (isset($baseUrl)) { $baseUrl = $baseUrl; } else { $baseUrl = '/media/pages' . $page->url() . '/'; }
 
 $image1x = $baseUrl . $filename . '.' .  $extension;
 $image2x = $baseUrl . $filename . '@2x.' .  $extension;
